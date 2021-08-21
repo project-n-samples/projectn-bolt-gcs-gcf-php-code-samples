9dd21b72e31984bc9e90037de21d900c485722d8
mpolu <mp@projectn.co>
2021-08-22 05:05:08 +0530

    Set response content type to application/json

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+header("Content-Type: application/json");
+
 include_once "boltGoogleCloudStorageOpsClientGCF.php";
 include_once "boltGoogleCloudStorageValidateObjGCF.php";
 include_once "boltGoogleCloudStoragePerfTestGCF.php";
